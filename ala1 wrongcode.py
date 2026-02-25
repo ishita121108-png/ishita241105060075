f9cf149f8b54bc348567b90d15e69e68516f731a
ishita121108-png <ishita121108@gmail.com>
2026-02-25 11:38:07 +0530

    Add files via upload

---
+++ ala1 wrongcode.py
@@ -0,0 +1,15 @@
+print("Weather Report")
+cities = ["Delhi","Mumbai","Pune"]
+temps = [30,32,28]
+city = input("Enter city: ")
+index = cities.index(city)
+if temps[index] > 25
+status = "Hot"
+else
+status = "Cool"
+print("City:", city)
+print("Temperature:", Temp)
+print("Status:", status)
+for i in range(3)
+print("Checked")
+print("End")
